f73cc64a9ef11146a8fb00d62ccb422337c05a28
David King <dking@xforty.com>
2012-01-03 10:34:40 -0500

    Update description and sort depends in alpha order
    - Updated the description in metadata.rb.
    - Placed all depends together without any comments or line breaks.
    - Sorted depends list in alphabetical order.

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,13 @@
 maintainer       "xforty technologies"
 maintainer_email "dking@xforty.com"
 license          "Apache 2.0"
-description      "Installs/Configures drupal"
+description      "Installs a drupal site"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
-# default recipe
-depends          "php"
-depends          "imagemagick"
 depends          "apache2"
-depends          "mysql"
 depends          "drush"
-
-# dev recipe
 depends          "git"
+depends          "imagemagick"
+depends          "mysql"
+depends          "php"
