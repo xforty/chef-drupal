3f407c90261c0eb2de86cf61b19459b7a0ad1a95
David King <dking@xforty.com>
2012-01-25 00:45:09 -0500

    Change metadata description

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "xforty technologies"
 maintainer_email "dking@xforty.com"
 license          "Apache 2.0"
-description      "Installs a drupal site"
+description      "Installs a drupal environment"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
